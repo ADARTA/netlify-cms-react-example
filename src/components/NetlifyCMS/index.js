172f2d97d88b77ed7420fd51093df230567d0f15
Tony Alves <tony@3alves.com>
2020-09-28 14:20:27 -0700

    proxy server api
    Use new proxy server instead of `netlify-cms-backend-fs` (deprecated)

--- src/components/NetlifyCMS/index.js
+++ src/components/NetlifyCMS/index.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import FileSystemBackend from 'netlify-cms-backend-fs';
-import CMS from 'netlify-cms-app';
-import previewStyles from './components/previewStyles';
-import PostPreview from './components/PostPreview';
-import AuthorsPreview from './components/AuthorsPreview';
-import GeneralPreview from './components/GeneralPreview';
-import EditorYoutube from './components/EditorYoutube';
-import RelationKitchenSinkPostPreview from './components/RelationKitchenSinkPostPreview';
+import React from "react";
+import CMS from "netlify-cms-app";
+import previewStyles from "./components/previewStyles";
+import PostPreview from "./components/PostPreview";
+import AuthorsPreview from "./components/AuthorsPreview";
+import GeneralPreview from "./components/GeneralPreview";
+import EditorYoutube from "./components/EditorYoutube";
+import RelationKitchenSinkPostPreview from "./components/RelationKitchenSinkPostPreview";
 
-import config from './config.json';
+import config from "./config.json";
 
 const NetlifyCMS = () => {
   React.useEffect(() => {
-    console.log(`CMS [${process.env.NODE_ENV}]`, CMS, )
-    if (process.env.NODE_ENV === 'development') {
-      config.load_config_file = false
-      config.backend = {
-        "name": "file-system",
-        "api_root": "http://localhost:3000/api"
-      }
-      CMS.registerBackend('file-system', FileSystemBackend);
+    console.log(`CMS [${process.env.NODE_ENV}]`, CMS);
+    if (process.env.NODE_ENV === "development") {
+      config.load_config_file = false;
+      config.local_backend = {
+        url: "http://localhost:3000/api/v1",
+      };
     }
     CMS.registerPreviewTemplate("posts", PostPreview);
     CMS.registerPreviewStyle(previewStyles, { raw: true });
     CMS.registerPreviewTemplate("authors", AuthorsPreview);
     CMS.registerPreviewTemplate("general", GeneralPreview);
     CMS.registerEditorComponent(EditorYoutube);
-    CMS.registerWidget("relationKitchenSinkPost", "relation", RelationKitchenSinkPostPreview);
+    CMS.registerWidget(
+      "relationKitchenSinkPost",
+      "relation",
+      RelationKitchenSinkPostPreview
+    );
 
-    CMS.init({config})
-  })
+    CMS.init({ config });
+  });
 
-  return <div id="nc-root" />
+  return <div id="nc-root" />;
 };
 
 export default NetlifyCMS;
